de831cc963072636a1e11c868fd5a2d2e143143b
Ledana <ledanagjoka66@gmail.com>
2026-05-06 12:58:31 +0200

    added try catch in every method of the controller

--- DrinksInfo.Ledana/DrinksInfo.Ledana/Controllers/FavouriteDrinkController.cs
+++ DrinksInfo.Ledana/DrinksInfo.Ledana/Controllers/FavouriteDrinkController.cs
@@ -7,38 +7,71 @@ namespace DrinksInfo.Ledana.Controllers
     {
         internal static void AddDrinkToFavourites(DrinkDetail favouriteDrink)
         {
-            using var context = new FavouritesDrinkContext();
-
-            context.FavouriteDrinks.Add(favouriteDrink);
-            context.SaveChanges();
+            try
+            {
+                using var context = new FavouritesDrinkContext();
+                context.FavouriteDrinks.Add(favouriteDrink);
+                context.SaveChanges();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something went wrong! " + e.Message);
+            }
         }
 
         internal static async Task DeleteAllFavourites()
         {
+            try
+            { 
             using var context = new FavouritesDrinkContext();
             context.FavouriteDrinks.RemoveRange(context.FavouriteDrinks);
             await context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong! " + e.Message);
+            }
         }
 
         internal static void DeleteFavourite(string? drink)
         {
-            using var context = new FavouritesDrinkContext();
-            var drinkToDelete = context.FavouriteDrinks.Where(d => d.idDrink == drink).First();
-            context.FavouriteDrinks.Remove(drinkToDelete);
-            context.SaveChanges();
-        }
+            try
+            {
+                using var context = new FavouritesDrinkContext();
+                var drinkToDelete = context.FavouriteDrinks.Where(d => d.idDrink == drink).First();
+                context.FavouriteDrinks.Remove(drinkToDelete);
+                context.SaveChanges();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Something went wrong! " + e.Message);
+            }
+}
 
         internal static List<DrinkDetail> GetFavouriteDrinks()
         {
-            using var context = new FavouritesDrinkContext();
-            return context.FavouriteDrinks.ToList();
+            try
+            {
+                using var context = new FavouritesDrinkContext();
+                return context.FavouriteDrinks.ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong! " + e.Message);
+            }
         }
 
         internal static bool HasDrink(string? drink)
         {
-            using var context = new FavouritesDrinkContext();
-
-            return context.FavouriteDrinks.Any(d => d.idDrink == drink);
+            try
+            {
+                using var context = new FavouritesDrinkContext();
+                return context.FavouriteDrinks.Any(d => d.idDrink == drink);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong! " + e.Message);
+            }
         }
     }
 }
